60b0f2e7393a2bd43fa3df272896e8a07db51f82
Phạm Anh Kiệt <kietpase183731@fpt.edu.vn>
2025-08-12 23:34:26 +0700

    Learning

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -1,16 +1,36 @@
+import React from 'react';
+
 const Counter = () => {
+  //useState can be any type, not just number
+  //2 useState because we want to keep the current count and the input value separate, temp var won't cause re-render
+  //useState returns an array with two elements: the current state and a function to update
+
+  const [count, setCount] = React.useState(0);
+  const [Acount, setAcount] = React.useState(count);
+
   return (
-    <section className="flex flex-col items-center w-2/3 gap-8 p-8 bg-white border-4 shadow-lg border-primary-500">
+    <section className="flex w-2/3 flex-col items-center gap-8 border-4 border-primary-500 bg-white p-8 shadow-lg">
       <h1>Days Since the Last Accident</h1>
-      <p className="text-6xl">0</p>
+      <p className="text-6xl">{count}</p> {/*the vaule here*/}
       <div className="flex gap-2">
-        <button>➖ Decrement</button>
-        <button>🔁 Reset</button>
-        <button>➕ Increment</button>
+        <button onClick={()=>setCount(count -1)}>➖ Decrement</button> {/*set the value with function and it auto re-render*/}
+        <button onClick={()=>setCount(0) }>🔁 Reset</button>
+        {/*we need arrow function or const when click/onclick,...to
+        1. passing vaule to function (setCount(count+1))
+        2. call function directly will trigger immediately, arrow function/const prevent that
+        3. const functions are event handler and re-create every render different from ReactBuilt-In and Component function*/}
+        <button onClick={()=>setCount(count+1)}>➕ Increment</button>
       </div>
       <div>
-        <form onSubmit={(e) => e.preventDefault()}>
-          <input type="number" value={0} />
+        <form onSubmit={(e) => {
+          e.preventDefault()
+          setCount(Acount);
+        }}>
+          <input
+            type="number"
+            value={Acount}
+            onChange={(e) => setAcount(Number(e.target.valueAsNumber))}
+          />
           <button type="submit">Update Counter</button>
         </form>
       </div>
